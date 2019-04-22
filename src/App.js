1b9a6b841cb4279eb951af9de9ef0c8d2ff55836
Gurdand <zmei83666@gmail.com>
2019-04-22 12:57:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import QuestionsList from './questions2.json';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		questions: QuestionsList.questions,
+		qurent_question: 1,
+		answer: 0,
+		active: 0,
+		correct: 0,
+		check: false
+	}
+
+	answerClick = (event) => {
+		let id = event.target.id;
+		this.setState({ answer: Number(id) })
+	}
+
+	checkAnswer = () => {
+		if(Number(QuestionsList.questions[this.state.qurent_question].correct_answer) === Number(this.state.answer)) {
+			this.setState( prevState => ({ correct: ++prevState.correct, check: true }) )
+		}else{
+			this.setState({ check: true })
+		}
+	}
+
+	nextQuestion = () => {
+		if(this.state.qurent_question === 96) {
+			this.setState({ 
+				qurent_question: 1,
+				check: false,
+				answer: 0
+			})
+		}else{
+			this.setState(prevState => ({
+				qurent_question: ++prevState.qurent_question,
+				check: false,
+				answer: 0
+			}));
+		}
+	}
+
+	check = (key) => {
+		if(!this.state.check && key+1 === this.state.answer) return "active";
+		if(this.state.check && key+1 === Number(QuestionsList.questions[this.state.qurent_question].correct_answer)) return "correct";
+		if((this.state.check && key+1 === this.state.answer) && key+1 !== Number(QuestionsList.questions[this.state.qurent_question].correct_answer)) return "wrong";
+		return "button";
+	}
+
+
+  	render() {
+		  
+		const { questions, qurent_question } = this.state;
+		  
+		return (
+			<div className="App">
+			<p>Правильных ответов { this.state.correct } из 96</p>
+			<p>Билет № {this.state.qurent_question}</p>
+				<div className="question">
+					<p>{ questions[qurent_question].question }</p>
+				</div>
+				<div className="answers">
+					{
+						questions[qurent_question].answers.map((answer,key) => 
+							<p className={ this.check(key) }
+							key={ key } id={ key+1 } onClick={this.answerClick}>{ answer }</p>
+						)
+					}
+				</div>
+				{
+					this.state.check ? <button onClick={this.nextQuestion}>NEXT</button> : <button onClick={this.checkAnswer}>Проверить</button>
+				}
+				
+			</div>
+		);
+  	}
 }
 
 export default App;
