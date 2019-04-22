f93ad643ca46a9e5917822230a3be5184fcfebff
Gurdand <zmei83666@gmail.com>
2019-04-22 13:39:50 +0300

    Added docs folder

---
+++ docs/precache-manifest.843e76ee13d983c80614dc505e4be728.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d6aeb7981bf20949a7",
+    "url": "/AppTest/static/js/runtime~main.41fe3109.js"
+  },
+  {
+    "revision": "86aa50881859183abce2",
+    "url": "/AppTest/static/js/main.e64c22f0.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/AppTest/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "86aa50881859183abce2",
+    "url": "/AppTest/static/css/main.945bffc2.chunk.css"
+  },
+  {
+    "revision": "0dd985e4fbcbc881c36d4f90f51b217c",
+    "url": "/AppTest/index.html"
+  }
+];
